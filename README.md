2e552c67e2b08e16cca689aa76df7f40cd95f223
AgusAlonso1 <agusalonso@itba.edu.ar>
2024-11-26 13:46:53 -0300

    Accept IPV4 and IPV6 conections. Updated Readme.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # tpe-protos
+Servidor de POP3 con servicio de management, implementado por Valentina Marti Reta, Tomas Becerra y Agustin Alonso.
+
+## Compilación
+Para compilar el proyecto ejecutar el siguiente comando en la base del repositorio.
+~~~
+make clean all
+~~~
+Esto generara dos ejecutables: serverx y mng. serverx es el ejecutable propio del servidor, asi como mng es la aplicación cliente del protocolo de management.
+
+## Uso
+Ejecutar los siguientes comandos en la base del repositorio para consultar los parametros que reciben los dos binarios.
+Para el servidor:
+~~~
+./serverx -h
+~~~
+Para el protocolo de management:
+~~~
+./mng -h
+~~~
+
+Este mismo utiliza un token de autorización es cual fue definido arbitrariamente de la siguiente manera: "aaaaaaaa".
--- src/main.c
+++ src/main.c
@@ -24,19 +24,25 @@ int main(int argc, char ** argv) {
     init_server_args(argc, argv);
     init_server_info();
     
-    pop3_passive_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    pop3_passive_socket = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
     if (pop3_passive_socket == ERROR_CODE) {
         error_msg = SOCKET_CREATION_ERROR_MSG;
         goto finally;
     }
 
+    int dual_stack = 0;
+
     setsockopt(pop3_passive_socket, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
 
-    struct sockaddr_in my_listen_addr;
+    // Permitir conexiones tanto IPV4 como IPV6
+    setsockopt(pop3_passive_socket, IPPROTO_IPV6, IPV6_V6ONLY, &dual_stack, sizeof(dual_stack));
+
+    struct sockaddr_in6 my_listen_addr;
     memset(&my_listen_addr, 0, sizeof(my_listen_addr));
-    my_listen_addr.sin_family = AF_INET;
-    my_listen_addr.sin_port = htons(get_pop3_port());
-    my_listen_addr.sin_addr.s_addr = inet_addr(get_pop3_addr());
+    my_listen_addr.sin6_family = AF_INET6;
+    my_listen_addr.sin6_port = htons(get_pop3_port());
+    inet_pton(AF_INET6, get_pop3_addr(), &my_listen_addr.sin6_addr);
+
     if (bind(pop3_passive_socket, (struct sockaddr *) &my_listen_addr, sizeof(my_listen_addr)) == ERROR_CODE) {
         error_msg = SOCKET_BINDING_ERROR_MSG;
         goto finally;
@@ -89,7 +95,7 @@ int main(int argc, char ** argv) {
         goto finally;
     }
 
-    manager_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    manager_socket = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
 
     if (manager_socket == ERROR_CODE) {
         error_msg = SOCKET_CREATION_ERROR_MSG;
@@ -97,12 +103,13 @@ int main(int argc, char ** argv) {
     } 
 
     setsockopt(manager_socket, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+    setsockopt(manager_socket, IPPROTO_IPV6, IPV6_V6ONLY, &dual_stack, sizeof(dual_stack));
 
-    struct sockaddr_in manager_listen_addr;
+    struct sockaddr_in6 manager_listen_addr;
     memset(&manager_listen_addr, 0, sizeof(manager_listen_addr));
-    manager_listen_addr.sin_family = AF_INET;
-    manager_listen_addr.sin_port = htons(get_manager_port());
-    manager_listen_addr.sin_addr.s_addr = inet_addr(get_manager_addr());
+    manager_listen_addr.sin6_family = AF_INET6;
+    manager_listen_addr.sin6_port = htons(get_manager_port());
+    inet_pton(AF_INET6, get_pop3_addr(), &my_listen_addr.sin6_addr);
 
     if (bind(manager_socket, (struct sockaddr *) &manager_listen_addr, sizeof(manager_listen_addr)) == ERROR_CODE) {
         error_msg = SOCKET_BINDING_ERROR_MSG;
